6ad5330aa1a6f026ccc31b8481a6de508bda6070
romanC5 <romandangol91@gmail.com>
2025-05-30 06:38:59 +0545

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <nav className=''>
+            <div className=''>
+                  
+                  <h1 className='bg-red text-4xl font-bold'>Eshop</h1>
+            </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Wrapper from '../Components/Wrapper'
+const Home = () => {
+  return (
+    <>
+      <Wrapper>
+
+      </Wrapper>
+
+    </>
+  )
+}
+
+export default Home
